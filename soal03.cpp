78c1e2c2ac03c61649076df3293c14f1a235f188
gustinurshobah <32300981+gustinurshobah@users.noreply.github.com>
2017-11-08 00:26:32 +0800

    menampilkan teks

--- soal03.cpp
+++ soal03.cpp
@@ -3,4 +3,9 @@ using namespace std;
 
 int main(){
 	float jmkrj, lmbur, gjtot, gjpkk, gjbrsh,uphlmbr, jam, pjk;
+	
+	cout<<"   PROGRAM GAJI BBERSIH KARYAWAN"<<endl;
+	cout<<""<<endl;
+	cout<<"Lama Kerja Karyawan    : "; cin>> jmkrj;
+	cout<<"Gaji Perjam Karyawan   : "; cin>>gjpkk;
 }
