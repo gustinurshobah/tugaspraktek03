edfc130958735810a4434eca22b51fa39c99811f
gustinurshobah <32300981+gustinurshobah@users.noreply.github.com>
2017-11-08 00:41:51 +0800

    memasukkan perintah menampilkan teks

--- soal05.cpp
+++ soal05.cpp
@@ -7,3 +7,10 @@ int main()
 	float bb;
     int tb;
     
+    cout<<"PROGRAM MENENTUKAN underweight, normal, atau overweight"<<endl;
+    cout<<"======================================================="<<endl;
+    cout << "Silahkan masukkan tinggi badan(cm) : "; cin >>tb;
+    cout << "Silahkan masukkan berat badan(kg) : " ;
+    cin >> bb;
+    
+}
