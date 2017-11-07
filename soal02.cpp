d9b79fee6a16eab0a2e7d1d5aebf79d529833a67
gustinurshobah <32300981+gustinurshobah@users.noreply.github.com>
2017-11-07 22:51:19 +0800

    menggunakan kondisi if

--- soal02.cpp
+++ soal02.cpp
@@ -12,4 +12,23 @@ int main(){
 	cout <<"s = segitiga"<<endl;
 	cout <<"ketikan huruf [p/s]";cin>>r;
 	cout << endl;
+	
+	if (r =='p'||r =='p'){
+		cout <<"menghitung Luas Persegi Panjang"<<endl;
+		cout <<"input nilai panjang : "; cin >> x;
+		cout <<"input nilai lebar : "; cin >> y;
+		L=x*y;
+		cout <<"Luas Persegi Panjang : " <<L<<"   cm";
+	}
+		else if(r =='s'||r =='s'){
+		cout <<"menghitung Luas Segitiga"<<endl;
+		cout <<"input nilai alas : "; cin >> x;
+		cout <<"input nilai tinggi : "; cin >> y;
+		L=0.5*x*y;
+		cout <<"Luas Segitiga: "<<L<<"   cm";
+	}
+	else{
+		cout <<"INPUT TIDAK VALID!";
+		
+	}
 }
